08b0efcb09fa0cc344340ab1353f3bdea010686e
instauseraditya <incogfeed@gmail.com>
2026-09-07 22:09:49 +0530

    this is the code

--- firebase-config.js
+++ firebase-config.js
@@ -8,35 +8,35 @@
 // they are not secret keys. Access is controlled by Firestore Security
 // Rules instead (see README.md).
 
-// window.firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_PROJECT_ID.appspot.com",
-//   messagingSenderId: "YOUR_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-// };
+window.firebaseConfig = {
+  apiKey: "AIzaSyATf1iXC-03LbTvQdZ7C9Tk54vkR8osL-s",
+  authDomain: "for-a-stranger.firebaseapp.com",
+  projectId: "for-a-stranger",
+  storageBucket: "for-a-stranger.firebasestorage.app",
+  messagingSenderId: "195222287285",
+  appId: "1:195222287285:web:7d653e613d7664fe1a5ffc",
+};
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyATf1iXC-03LbTvQdZ7C9Tk54vkR8osL-s",
-  authDomain: "for-a-stranger.firebaseapp.com",
-  databaseURL: "https://for-a-stranger-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "for-a-stranger",
-  storageBucket: "for-a-stranger.firebasestorage.app",
-  messagingSenderId: "195222287285",
-  appId: "1:195222287285:web:7d653e613d7664fe1a5ffc",
-  measurementId: "G-HJN0CYN788"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyATf1iXC-03LbTvQdZ7C9Tk54vkR8osL-s",
+//   authDomain: "for-a-stranger.firebaseapp.com",
+//   databaseURL: "https://for-a-stranger-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "for-a-stranger",
+//   storageBucket: "for-a-stranger.firebasestorage.app",
+//   messagingSenderId: "195222287285",
+//   appId: "1:195222287285:web:7d653e613d7664fe1a5ffc",
+//   measurementId: "G-HJN0CYN788"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
